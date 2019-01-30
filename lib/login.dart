4c04d76a91ffd510b2dd3a5076e35e9efd6d66a9
davechao <dave.chao@silkrode.com.tw>
2019-01-30 16:29:51 +0800

    1. refactor login page 2. add home page

--- lib/login.dart
+++ lib/login.dart
@@ -11,6 +11,51 @@ class _LoginPageState extends State<LoginPage> {
   final _usernameController = TextEditingController();
   final _passwordController = TextEditingController();
 
+  Widget _buildLogo() {
+    return Column(
+      children: <Widget>[
+        Image.asset('assets/diamond.png'),
+        SizedBox(height: 16.0),
+        Text('SHRINE'),
+      ],
+    );
+  }
+
+  Widget _buildUserNameTextField() {
+    return TextField(
+      controller: _usernameController,
+      decoration: InputDecoration(
+        filled: true,
+        labelText: 'Username',
+      ),
+    );
+  }
+
+  Widget _buildPasswordTextField() {
+    return TextField(
+      controller: _passwordController,
+      decoration: InputDecoration(
+        filled: true,
+        labelText: 'Password',
+      ),
+      obscureText: true,
+    );
+  }
+
+  Widget _buildCancelButton() {
+    return FlatButton(
+      child: Text('CANCEL'),
+      onPressed: _clearData,
+    );
+  }
+
+  Widget _buildNextButton() {
+    return RaisedButton(
+      child: Text('NEXT'),
+      onPressed: _goHomePage,
+    );
+  }
+
   _clearData() {
     _usernameController.clear();
     _passwordController.clear();
@@ -27,47 +72,16 @@ class _LoginPageState extends State<LoginPage> {
         child: ListView(
           padding: EdgeInsets.symmetric(horizontal: 24.0),
           children: <Widget>[
-            SizedBox(
-              height: 80.0,
-            ),
-            Column(
-              children: <Widget>[
-                Image.asset('assets/diamond.png'),
-                SizedBox(height: 16.0),
-                Text('SHRINE'),
-              ],
-            ),
-            SizedBox(
-              height: 120.0,
-            ),
-            TextField(
-              controller: _usernameController,
-              decoration: InputDecoration(
-                filled: true,
-                labelText: 'Username',
-              ),
-            ),
-            SizedBox(
-              height: 12.0,
-            ),
-            TextField(
-              controller: _passwordController,
-              decoration: InputDecoration(
-                filled: true,
-                labelText: 'Password',
-              ),
-              obscureText: true,
-            ),
+            SizedBox(height: 80.0),
+            _buildLogo(),
+            SizedBox(height: 120.0),
+            _buildUserNameTextField(),
+            SizedBox(height: 12.0),
+            _buildPasswordTextField(),
             ButtonBar(
               children: <Widget>[
-                FlatButton(
-                  child: Text('CANCEL'),
-                  onPressed: _clearData,
-                ),
-                RaisedButton(
-                  child: Text('NEXT'),
-                  onPressed: _goHomePage,
-                ),
+                _buildCancelButton(),
+                _buildNextButton(),
               ],
             ),
           ],
